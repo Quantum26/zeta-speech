eaf29ce200001d8e9dc74116403dc55c5eab17b6
Ian <iansong26@gmail.com>
2023-06-21 20:21:43 -0500

    added setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import os
 path_to_folder = os.path.dirname(__file__)
 path = os.path.join(path_to_folder, "run_speech_commands.lnk")
-import winshell
+try:
+    from winshell import shortcut
+except ImportError:
+    raise ImportError("Dependencies not installed. Run 'pip install -r requirements.txt' and run setup.py again.")
 
 arg_str = "/k cd " + path_to_folder + " & python main.py & exit"
 
-# Create the shortcut on the desktop
-with winshell.shortcut(path) as link:
+# Create the shortcut in the folder
+with shortcut(path) as link:
     link.path = "C:\\Windows\\System32\\cmd.exe"
     link.description = "Run the speech commands program."
     link.arguments = arg_str
